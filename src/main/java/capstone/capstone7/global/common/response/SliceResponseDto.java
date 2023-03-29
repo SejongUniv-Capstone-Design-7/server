ec7cccb26c6982dadc9191d3c89f57e4fdead21f
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-03-30 01:16:45 +0900

    [ADD/#6] BaseResponseDto, SliceResponseDto 클래스 생성

---
+++ src/main/java/capstone/capstone7/global/common/response/SliceResponseDto.java
@@ -0,0 +1,25 @@
+package capstone.capstone7.global.common.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Slice;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SliceResponseDto<T> implements Serializable {
+    private List<T> content;
+    private boolean hasNext;
+    private boolean isFirst;
+    private boolean isLast;
+
+    public SliceResponseDto(Slice<T> slice){
+        this.content = slice.getContent();
+        this.hasNext = slice.hasNext();
+        this.isFirst = slice.isFirst();
+        this.isLast = slice.isLast();
+    }
+}
