6ea14574828c2bf6936b8d7a0d6a4cd6d3dc3f44
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-03-30 00:01:55 +0900

    [ADD/#1] BoardStorage 엔티티 생성

---
+++ src/main/java/capstone/capstone7/domain/storage/entity/BoardStorage.java
@@ -0,0 +1,27 @@
+package capstone.capstone7.domain.storage.entity;
+
+import capstone.capstone7.domain.board.entity.Board;
+import capstone.capstone7.domain.Member.entity.Member;
+import capstone.capstone7.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+public class BoardStorage extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+}
