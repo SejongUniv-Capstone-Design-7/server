d781461dbae7ab4a233fd0f9220e0e2b41826899
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-03-30 00:00:57 +0900

    [ADD/#1] BoardLike 엔티티 생성

---
+++ src/main/java/capstone/capstone7/domain/like/entity/BoardLike.java
@@ -0,0 +1,26 @@
+package capstone.capstone7.domain.like.entity;
+
+import capstone.capstone7.domain.board.entity.Board;
+import capstone.capstone7.domain.Member.entity.Member;
+import capstone.capstone7.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+public class BoardLike extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
