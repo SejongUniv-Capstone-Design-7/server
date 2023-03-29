a4a21f39bf84211f5e063b4a1feb315af4b0cdeb
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-03-30 00:00:35 +0900

    [ADD/#1] Comment 엔티티 생성

---
+++ src/main/java/capstone/capstone7/domain/comment/entity/Comment.java
@@ -0,0 +1,28 @@
+package capstone.capstone7.domain.comment.entity;
+
+import capstone.capstone7.domain.board.entity.Board;
+import capstone.capstone7.domain.Member.entity.Member;
+import capstone.capstone7.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
