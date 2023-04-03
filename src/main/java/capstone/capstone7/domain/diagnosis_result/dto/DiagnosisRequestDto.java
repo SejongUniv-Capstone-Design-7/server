6394557d16ff4541acf37b3e7bd5041c2fc345fa
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-03 17:47:16 +0900

    [FEAT/#3] DiagnosisResult | 병해충 검출에 사용할 DTO 생성

---
+++ src/main/java/capstone/capstone7/domain/diagnosis_result/dto/DiagnosisRequestDto.java
@@ -0,0 +1,9 @@
+package capstone.capstone7.domain.diagnosis_result.dto;
+
+import lombok.Getter;
+
+@Getter
+public class DiagnosisRequestDto {
+    private String crop_sort;
+}
+
---
+++ src/main/java/capstone/capstone7/domain/diagnosis_result/dto/DiagnosisResponseDto.java
@@ -0,0 +1,9 @@
+package capstone.capstone7.domain.diagnosis_result.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DiagnosisResponseDto {
+}
