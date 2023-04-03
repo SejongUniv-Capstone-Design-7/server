19bbd0019027ec6f500d2af73c3cc2a2ecef6258
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-03 16:15:27 +0900

    [ADD/#1] @EnableJpaAuditing 애노테이션 추가
    JPA Auditing 애노테이션 활성화

--- src/main/java/capstone/capstone7/Capstone7Application.java
+++ src/main/java/capstone/capstone7/Capstone7Application.java
@@ -2,7 +2,9 @@ package capstone.capstone7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Capstone7Application {
 
