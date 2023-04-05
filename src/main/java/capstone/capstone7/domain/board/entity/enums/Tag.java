1729785ebe54ad9d84467fc1cfcc00354312bea2
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-06 00:52:42 +0900

    [ADD/#12] RequestDto에서 Enum타입 Json변환을 위한 @JsonCreator 코드 추가

--- src/main/java/capstone/capstone7/domain/board/entity/enums/Tag.java
+++ src/main/java/capstone/capstone7/domain/board/entity/enums/Tag.java
@@ -1,5 +1,20 @@
 package capstone.capstone7.domain.board.entity.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+
 public enum Tag {
     KNOWHOW, QUESTION;
+
+    // DTO에서 ENUM 타입을 받고, KNOWHOW, QUESTION이 아닌 값이 들어왔다면 null 반환
+    // 나중에 @Valid를 통해서 null validation 필요
+    @JsonCreator(mode=JsonCreator.Mode.DELEGATING)
+    public static Tag get(String inputTag) {
+        return Arrays.stream(values())
+                .filter(tag -> tag.toString().equals(inputTag))
+                .findAny()
+                .orElse(null);
+    }
+
 }
