f3d7695944bde8329a792f98ee0a1ec6d0118dfc
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-07 01:23:45 +0900

    [FEAT/#10] 이메일로 DB에서 회원 찾기 구현

--- src/main/java/capstone/capstone7/domain/Member/repository/MemberRepository.java
+++ src/main/java/capstone/capstone7/domain/Member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package capstone.capstone7.domain.Member.repository;
 import capstone.capstone7.domain.Member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
