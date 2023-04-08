66e10024c8c30badd08d0c3611c55d4339898c6a
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-08 20:09:27 +0900

    [FEAT/#19] 게시글 목록 조회 기능 구현

--- src/main/java/capstone/capstone7/domain/board/controller/BoardController.java
+++ src/main/java/capstone/capstone7/domain/board/controller/BoardController.java
@@ -2,11 +2,14 @@ package capstone.capstone7.domain.board.controller;
 
 import capstone.capstone7.domain.board.dto.request.BoardCreateRequestDto;
 import capstone.capstone7.domain.board.dto.response.BoardCreateResponseDto;
+import capstone.capstone7.domain.board.dto.response.GetBoardResponseDto;
 import capstone.capstone7.domain.board.service.BoardService;
 import capstone.capstone7.global.auth.entity.LoginUser;
 import capstone.capstone7.global.common.response.BaseResponseDto;
+import capstone.capstone7.global.common.response.SliceResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -26,4 +29,10 @@ public class BoardController {
     public BaseResponseDto<BoardCreateResponseDto> createBoard(@RequestPart(value = "request") BoardCreateRequestDto boardCreateRequestDto, @RequestParam(value = "file", required = false) MultipartFile boardImage, @AuthenticationPrincipal LoginUser loginUser){
         return new BaseResponseDto<>(boardService.createBoard(boardImage, boardCreateRequestDto, loginUser.getMember()));
     }
+
+    @GetMapping()
+    public SliceResponseDto<GetBoardResponseDto> getBoardsList(Pageable pageable){
+        return new SliceResponseDto<>(boardService.getBoardsList(pageable));
+    }
+
 }
