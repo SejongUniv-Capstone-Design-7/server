07ebe6886f107f1b12ed8e0fd6dcd109813e5f4f
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-11 12:51:26 +0900

    [ADD/#10] Auth / filter 에러처리 filter 생성

--- src/main/java/capstone/capstone7/global/auth/filter/JwtAccessDeniedHandler.java
+++ src/main/java/capstone/capstone7/global/auth/filter/JwtAccessDeniedHandler.java
@@ -1,19 +1,25 @@
 package capstone.capstone7.global.auth.filter;
 
+import capstone.capstone7.global.error.exception.custom.AuthException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static capstone.capstone7.global.error.enums.ErrorMessage.FORBIDDEN;
+
+@Slf4j
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         //필요한 권한이 없이 접근하려 할때 403
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        log.info("AccessDeniedHandler: 403 에러 호출");
+        throw new AuthException(FORBIDDEN);
     }
 }
