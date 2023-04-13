6dfbcf7e2cbfc47a3a9b2e1344db3d84510a6313
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-13 16:34:23 +0900

    [FEAT/#42] Like / 게시글 좋아요 삭제 기능 구현

--- src/main/java/capstone/capstone7/domain/like/service/LikeService.java
+++ src/main/java/capstone/capstone7/domain/like/service/LikeService.java
@@ -4,6 +4,7 @@ import capstone.capstone7.domain.Member.entity.Member;
 import capstone.capstone7.domain.Member.repository.MemberRepository;
 import capstone.capstone7.domain.board.entity.Board;
 import capstone.capstone7.domain.board.repository.BoardRepository;
+import capstone.capstone7.domain.like.dto.response.BoardLikeDeleteResponseDto;
 import capstone.capstone7.domain.like.dto.response.BoardLikePostResponseDto;
 import capstone.capstone7.domain.like.entity.BoardLike;
 import capstone.capstone7.domain.like.repository.LikeRepository;
@@ -15,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static capstone.capstone7.global.error.enums.ErrorMessage.ALREADY_DELETED_BOARDLIKE;
 import static capstone.capstone7.global.error.enums.ErrorMessage.ALREADY_EXIST_BOARDLIKE;
 
 @Slf4j
@@ -44,5 +46,23 @@ public class LikeService {
         likeRepository.save(newBoardLike);
 
         return new BoardLikePostResponseDto(board.getId(), board.getLikeNum());
+    }
+
+    public BoardLikeDeleteResponseDto deleteLike(Long boardId, LoginUser loginUser){
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new BusinessException(ErrorMessage.NOT_EXIST_BOARD));
+        board.addLike();
+
+        Member member = memberRepository.findById(loginUser.getMember().getId()).orElseThrow(() -> new BusinessException(ErrorMessage.NOT_EXIST_USER));
+
+        // 이미 해당 게시물에 좋아요 취소를 눌렀다면 에러처리
+        if(!likeRepository.existsByBoardAndMember(board, member)){
+            throw new BusinessException(ALREADY_DELETED_BOARDLIKE);
+        }
+
+        BoardLike boardLike = likeRepository.findByBoardAndMember(board, member).orElseThrow(() -> new BusinessException(ALREADY_DELETED_BOARDLIKE));
+
+        likeRepository.deleteById(boardLike.getId());
+
+        return new BoardLikeDeleteResponseDto(board.getId(), board.getLikeNum());
     }
 }
