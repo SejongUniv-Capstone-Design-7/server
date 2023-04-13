63c00f4c7db25b734fea47e4893aece6e7737202
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-13 16:34:23 +0900

    [FEAT/#42] 게시글 좋아요 삭제 기능 구현

--- src/main/java/capstone/capstone7/domain/like/controller/LikeController.java
+++ src/main/java/capstone/capstone7/domain/like/controller/LikeController.java
@@ -1,5 +1,6 @@
 package capstone.capstone7.domain.like.controller;
 
+import capstone.capstone7.domain.like.dto.response.BoardLikeDeleteResponseDto;
 import capstone.capstone7.domain.like.dto.response.BoardLikePostResponseDto;
 import capstone.capstone7.domain.like.service.LikeService;
 import capstone.capstone7.global.auth.entity.LoginUser;
@@ -7,10 +8,7 @@ import capstone.capstone7.global.common.response.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,5 +20,10 @@ public class LikeController {
     @PostMapping("")
     public BaseResponseDto<BoardLikePostResponseDto> addLike(@PathVariable Long boardId, @AuthenticationPrincipal LoginUser loginUser){
         return new BaseResponseDto<>(likeService.addLike(boardId, loginUser));
+    }
+
+    @DeleteMapping("")
+    public BaseResponseDto<BoardLikeDeleteResponseDto> deleteLike(@PathVariable Long boardId, @AuthenticationPrincipal LoginUser loginUser){
+        return new BaseResponseDto<>(likeService.deleteLike(boardId, loginUser));
     }
 }
--- src/main/java/capstone/capstone7/domain/like/repository/LikeRepository.java
+++ src/main/java/capstone/capstone7/domain/like/repository/LikeRepository.java
@@ -5,6 +5,9 @@ import capstone.capstone7.domain.board.entity.Board;
 import capstone.capstone7.domain.like.entity.BoardLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<BoardLike, Long> {
     Boolean existsByBoardAndMember(Board board, Member member);
+    Optional<BoardLike> findByBoardAndMember(Board board, Member member);
 }
