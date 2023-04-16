cdd372b8af596383b73a8c3f5fc81edbf2f52eb8
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-16 15:05:49 +0900

    [ADD/#19] Board / 게시글 조회시, 사용자 닉네임, 좋아요 개수도 반환

--- src/main/java/capstone/capstone7/domain/board/repository/BoardRepository.java
+++ src/main/java/capstone/capstone7/domain/board/repository/BoardRepository.java
@@ -4,8 +4,13 @@ import capstone.capstone7.domain.board.entity.Board;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query("select b from Board b join fetch b.member")
     Slice<Board> findBoardListBy(Pageable pageable);
+
 
 }
