3a2dbf9b6fe4af1d9c9c7c33864d458af4443b5b
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-04-27 00:10:51 +0900

    [FEAT/#99] Board / 게시글 validation 추가

--- src/main/java/capstone/capstone7/domain/board/dto/request/BoardCreateRequestDto.java
+++ src/main/java/capstone/capstone7/domain/board/dto/request/BoardCreateRequestDto.java
@@ -1,11 +1,21 @@
 package capstone.capstone7.domain.board.dto.request;
 
 import capstone.capstone7.domain.board.entity.enums.Tag;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class BoardCreateRequestDto {
+    @NotBlank(message = "게시글 제목은 필수 입력 값입니다.")
+    @Size(max=30, message = "게시글 제목은 30자 이하여야합니다.")
     private String title;
+
+    @NotBlank(message = "게시글 내용은 필수 입력 값입니다.")
+    @Size(max=500, message = "게시글 내용은 500자 이하여야합니다.")
     private String content;
-    private Tag tag; // 이게 가능한가
+
+    @NotNull(message = "태그는 KNOWHOW와 QUESTION 중에서 하나를 필수로 입력해야합니다.")
+    private Tag tag;
 }
--- src/main/java/capstone/capstone7/domain/board/dto/request/BoardUpdateRequestDto.java
+++ src/main/java/capstone/capstone7/domain/board/dto/request/BoardUpdateRequestDto.java
@@ -1,11 +1,21 @@
 package capstone.capstone7.domain.board.dto.request;
 
 import capstone.capstone7.domain.board.entity.enums.Tag;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class BoardUpdateRequestDto {
+    @NotBlank(message = "게시글 제목은 필수 입력 값입니다.")
+    @Size(max=30, message = "게시글 제목은 30자 이하여야합니다.")
     private String title;
+
+    @NotBlank(message = "게시글 내용은 필수 입력 값입니다.")
+    @Size(max=500, message = "게시글 내용은 500자 이하여야합니다.")
     private String content;
+
+    @NotNull(message = "태그는 KNOWHOW와 QUESTION 중에서 하나를 필수로 입력해야합니다.")
     private Tag tag;
 }
