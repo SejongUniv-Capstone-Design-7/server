a1b5e4b805037f91c5d3c0b63ae86c30136a2225
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-05-04 12:46:21 +0900

    [MOD/#109] Diagnosis / AI서버로 보낼 이미지 데이터 형식 file에서 MultipartFile로 수정

--- src/main/java/capstone/capstone7/domain/diagnosis_result/service/DiagnosisResultService.java
+++ src/main/java/capstone/capstone7/domain/diagnosis_result/service/DiagnosisResultService.java
@@ -8,17 +8,15 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.File;
-import java.io.IOException;
-
 import static capstone.capstone7.global.error.enums.ErrorMessage.EMPTY_FILE;
-import static capstone.capstone7.global.error.enums.ErrorMessage.FAIL_TO_SEND_AISERVER;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,49 +30,42 @@ public class DiagnosisResultService {
     private String aiServerUrl;
     @PostConstruct
     public void initWebClient() {
-        //webClient = WebClient.create("https://jsonplaceholder.typicode.com");
         webClient = WebClient.create(aiServerUrl);
     }
 
     public DiagnosisResultFromAIServer diagnosis(MultipartFile cropImage, DiagnosisRequestDto diagnosisRequestDto){
-
         if(cropImage.isEmpty()){
             throw new BusinessException(EMPTY_FILE);
         }
 
-        try{
-            MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        // fileService.uploadFileToLocal(cropImage, diagnosisRequestDto.getCrop_sort());
+        // File crop_image_file = fileService.convertFromMultipartToFile(cropImage);
+        String fileName = fileService.getDiagnosisFileNameSendToAIServer(cropImage, diagnosisRequestDto.getCrop_sort());
+        log.info("filename : {}", fileName);
 
-            File crop_image_file = fileService.convertFromMultipartToFile(cropImage);
-            String fileName = fileService.getDiagnosisFileNameSendToAIServer(diagnosisRequestDto.getCrop_sort());
-            log.info("filename : {}", fileName);
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part("crop_image", cropImage).filename(fileName);
+        multipartBodyBuilder.part("crop_sort", diagnosisRequestDto.getCrop_sort());
 
-            return null;
-            /*
-            multipartBodyBuilder.part("crop_image", new InputStreamResource(new FileInputStream(crop_image_file))).filename(fileName);
-            multipartBodyBuilder.part("crop_sort", diagnosisRequestDto.getCrop_sort());
+        DiagnosisResultFromAIServer diagnosisResultFromAIServer = webClient.post()
+                .uri("/model")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromMultipartData(multipartBodyBuilder.build()))
+                .retrieve()
+                .bodyToMono(DiagnosisResultFromAIServer.class)
+                .doOnError(e -> log.error("Mapping Error : ", e))
+                .block();
 
 
-            DiagnosisResultFromAIServer diagnosisResultFromAIServer = webClient.post()
-                    .uri("/model")
-                    .contentType(MediaType.MULTIPART_FORM_DATA)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .body(BodyInserters.fromMultipartData(multipartBodyBuilder.build()))
-                    .retrieve()
-                    .bodyToMono(DiagnosisResultFromAIServer.class)
-                    .doOnError(e -> log.error("Mapping Error : ", e))
-                    .block();
-*/
-            // 받아온 정보로 diagnosis Result에 저장
+        // 받아온 정보로 diagnosis Result에 저장
             /*DiagnosisResult.builder()
                     .member()
                     .diseaseName()
                     .build();*/
 
-            //return diagnosisResultFromAIServer;
-        }catch (IOException e){
-            throw new BusinessException(FAIL_TO_SEND_AISERVER);
-        }
+        return diagnosisResultFromAIServer;
+
     }
 
     // 받아온 정보 중에서 가장 probability가 높은 질병 추출
