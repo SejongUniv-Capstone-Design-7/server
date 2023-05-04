ddd3746c59ff8e56a3e470d0896d7970801b02a7
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-05-04 16:00:38 +0900

    [FEAT/#132] Board / 게시글 해결 완료 수정 기능 구현을 위한 DTO생성

---
+++ src/main/java/capstone/capstone7/domain/board/dto/response/BoardSolvedUpdateResponseDto.java
@@ -0,0 +1,10 @@
+package capstone.capstone7.domain.board.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardSolvedUpdateResponseDto {
+    private Long boardId;
+}
