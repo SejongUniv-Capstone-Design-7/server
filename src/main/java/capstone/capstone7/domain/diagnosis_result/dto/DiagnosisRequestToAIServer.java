1b29c3b44296c50a0603327f64c1e965cf0ed67e
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-05-06 00:16:35 +0900

    [MOD/#109] Diagnosis / AI 서버 연결도중 발생한 jackson 에러 해결을 위한 DTO 생성 및 수정

---
+++ src/main/java/capstone/capstone7/domain/diagnosis_result/dto/DiagnosisRequestToAIServer.java
@@ -0,0 +1,10 @@
+package capstone.capstone7.domain.diagnosis_result.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DiagnosisRequestToAIServer {
+    private String crop_sort;
+}
